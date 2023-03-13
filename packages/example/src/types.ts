db507b93dbcd135e91d0062956dbe7a87ee9d723
Luis Marsiglia <marsiglia.business@gmail.com>
2023-03-13 14:17:03 -0400

    chore: run prettier

--- packages/example/src/types.ts
+++ packages/example/src/types.ts
@@ -1,12 +1,12 @@
 import { langOptions, secretOptions, siteKeyOptions, sizeOptions } from './constants'
 
-export type SiteKeyType = typeof siteKeyOptions[number]['value']
+export type SiteKeyType = (typeof siteKeyOptions)[number]['value']
 
-export type SecretKeyType = typeof secretOptions[number]['value']
+export type SecretKeyType = (typeof secretOptions)[number]['value']
 
-export type WidgetSize = typeof sizeOptions[number]['value']
+export type WidgetSize = (typeof sizeOptions)[number]['value']
 
-export type Lang = typeof langOptions[number]['value']
+export type Lang = (typeof langOptions)[number]['value']
 
 export type Theme = 'light' | 'dark' | 'auto'
 
