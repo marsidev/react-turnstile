3035dc878c2208c26b55d2ba0c9d30c0d8e50dd0
Luis Marsiglia <marsiglia.business@gmail.com>
2023-03-25 15:26:03 -0400

    chore: extends global prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: false,
+	useTabs: true,
+	singleQuote: true,
+	jsxSingleQuote: false,
+	trailingComma: 'none',
+	tabWidth: 2,
+	arrowParens: 'avoid',
+	printWidth: 100
+}
--- demos/nextjs-app-router/.prettierrc.js
+++ demos/nextjs-app-router/.prettierrc.js
@@ -1,11 +1,4 @@
 module.exports = {
-	semi: false,
-	useTabs: true,
-	singleQuote: true,
-	jsxSingleQuote: false,
-	trailingComma: 'none',
-	tabWidth: 2,
-	arrowParens: 'avoid',
-	printWidth: 100,
+	...require('../../.prettierrc.js'),
 	plugins: [require('prettier-plugin-tailwindcss')]
 }
