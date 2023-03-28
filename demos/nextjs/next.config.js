197a9c2a379c7560dfdeb5b88c59fd003446a775
Luis Marsiglia <marsiglia.business@gmail.com>
2023-03-27 21:55:24 -0400

    chore: update next config

--- demos/nextjs/next.config.js
+++ demos/nextjs/next.config.js
@@ -1,17 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
 	experimental: {
 		appDir: true
-	},
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination: '/demo',
-				permanent: true
-			}
-		]
 	}
+	// async redirects() {
+	// 	return [
+	// 		{
+	// 			source: '/',
+	// 			destination: '/demo',
+	// 			permanent: true
+	// 		}
+	// 	]
+	// }
 }
 
 module.exports = nextConfig
