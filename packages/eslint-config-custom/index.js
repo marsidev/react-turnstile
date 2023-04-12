db9fd6d1b61fe3b6d7f5511c73763276c24655ee
Luis Marsiglia <marsiglia.business@gmail.com>
2023-04-12 14:40:19 -0400

    chore: add `eslint-plugin-react-hooks`

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-	extends: ['marsi/react-ts', 'prettier'],
+	extends: ['plugin:react-hooks/recommended', 'marsi/react-ts', 'prettier'],
 	rules: {
 		'no-control-regex': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
