805335b0356ac7183593a93b432d522f56ee03ba
Luis Marsiglia <marsiglia.business@gmail.com>
2023-08-31 20:07:08 -0400

    chore: Improve demos

--- demos/nextjs/src/app/manual-script-injection-with-custom-script-props/page.tsx
+++ demos/nextjs/src/app/manual-script-injection-with-custom-script-props/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Script from 'next/script'
 import { SCRIPT_URL } from '@marsidev/react-turnstile'
 import React from 'react'
@@ -6,7 +8,8 @@ import DemoWidget from '~/components/demo-widget'
 export default function Page() {
 	return (
 		<React.Fragment>
-			<Script id="turnstile-script" src={SCRIPT_URL} strategy="afterInteractive" />
+			{/* We add a custom query param to the script URL to force a re-download of the script, since the manual script injection is also used in other demos. This is not needed if the script ID is the same. */}
+			<Script id="turnstile-script" src={`${SCRIPT_URL}?v=2`} strategy="afterInteractive" />
 
 			<h1>Manual script injection with custom script props</h1>
 
