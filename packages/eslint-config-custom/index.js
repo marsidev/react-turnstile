5905998f5d94693a41e70c8fbece1490e5e9c624
Luis Marsiglia <marsiglia.business@gmail.com>
2025-12-08 16:51:15 -0400

    chore: eslint config changes

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	extends: ['marsi/react-ts', 'prettier'],
 	rules: {
+		'import/no-unresolved': 'off',
 		'no-control-regex': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
 		'jsx-quotes': ['warn', 'prefer-double'],
