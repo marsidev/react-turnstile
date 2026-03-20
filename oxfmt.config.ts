02368af3bd0a77e99f73c3f12770ca48d572c6af
Luis Marsiglia <marsiglia.business@gmail.com>
2026-03-20 19:07:12 -0400

    chore: add oxc tolling, remove eslint and prettier, add tsgo(preview)

---
+++ oxfmt.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+	semi: true,
+	useTabs: false,
+	singleQuote: false,
+	jsxSingleQuote: false,
+	trailingComma: 'none',
+	tabWidth: 2,
+	arrowParens: 'avoid',
+	printWidth: 100,
+	ignorePatterns: ['.vscode', 'dist', '.next', '*.yaml', '*.yml', '*.css', '*.md', '*.mdx']
+})
